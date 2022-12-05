5bc22eccaa196d238c1bd4be25eabce64321bceb
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-12-05 14:21:34 +0000

    first data structures commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
 	"""Print all integers of a list"""
 	def print_list_integer(my_list=[]):
-		for i in my_list:
-			print("{}".format(i))
+		for i in range(len(my_list)):
+			print("{:d}".format(my_list[i]))
 
