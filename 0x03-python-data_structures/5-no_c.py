b0dbcab54577ee26342384d05d641a6bc027f1bc
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-12-05 17:58:22 +0000

    remove C character from my_string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-	new_str = [i for i in my_string if i != 'c' or i != 'C']
+	new_str = [i for i in my_string if 'c' != i or 'C' != i]
 	return ("".join(new_str))
